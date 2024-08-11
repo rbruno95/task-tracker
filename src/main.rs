54de352b90017f012725cef09fb99991f9c903d3
Bruno <romariobruno95@gmail.com>
2024-08-11 20:44:23 +0200

    add cli-skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,32 @@
-fn main() {}
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Add { value: String },
+    Update { index: usize, new_value: String },
+    Delete { index: usize },
+    MarkInProgress { index: usize },
+    MarkDone { index: usize },
+    List { status: Option<String> },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Command::Add { value }) => {}
+        Some(Command::Update { index, new_value }) => {}
+        Some(Command::Delete { index }) => {}
+        Some(Command::MarkInProgress { index }) => {}
+        Some(Command::MarkDone { index }) => {}
+        Some(Command::List { status }) => {}
+        None => {}
+    }
+}
